412b8267f6689417527ec2c300e290cd2825259f
Jason Orendorff <jorendorff@github.com>
2023-05-13 07:53:19 -0500

    more tests!

--- playground/myml_test.py
+++ playground/myml_test.py
@@ -4,7 +4,7 @@ from numpy.random import default_rng
 import myml
 
 
-def test_layer(rng, layer, input_shape):
+def test_layer(rng, layer, input_shape, *, error_limit=1e-5):
     n = layer.num_params()
     params = rng.uniform(size=(n,))
     x = rng.uniform(size=input_shape)
@@ -14,37 +14,45 @@ def test_layer(rng, layer, input_shape):
     dp = np.zeros((n,))
     dx = layer.derivatives(params, x, dz, dp)
 
-    e = 1e-6
+    h = 1e-7
+
+    def err(claimed, measured):
+        # How bad is the claimed value?
+        denom = max(abs(measured), 1e-3)
+        return abs((claimed - measured) / denom)
 
     for i in range(n):
         # check accuracy of derivative at parameter i
         saved = params[i]
-        params[i] = saved - e
+        params[i] = saved - h
         z_minus = layer.apply(params, x)
-        params[i] = saved + e
+        params[i] = saved + h
         z_plus = layer.apply(params, x)
         params[i] = saved
 
         claimed = dp[i]
-        measured = np.sum((z_plus - z_minus) / (2 * e) * dz)
-        error = (claimed - measured) / measured
-        assert abs(error) < 1e-5
+        measured = np.sum((z_plus - z_minus) / (2 * h) * dz)
+
+        error = err(claimed, measured)
+        if error > error_limit:
+            raise ValueError(f"parameter {i} computed derivative = {claimed}, measured = {measured}, error = {error}, limit = {error_limit}")
 
     it = np.nditer(x, flags=['multi_index'])
     for _ in it:
         i = it.multi_index
         # check accuracy of derivative at input i
         saved = x[i]
-        x[i] = saved - e
+        x[i] = saved - h
         z_minus = layer.apply(params, x)
-        x[i] = saved + e
+        x[i] = saved + h
         z_plus = layer.apply(params, x)
         x[i] = saved
 
         claimed = dx[i]
-        measured = np.sum((z_plus - z_minus) / (2 * e) * dz)
-        error = (claimed - measured) / measured
-        assert abs(error) < 1e-5
+        measured = np.sum((z_plus - z_minus) / (2 * h) * dz)
+        error = err(claimed, measured)
+        if error > error_limit:
+            raise ValueError(f"input element {i} computed derivative = {claimed}, measured = {measured}, error = {error}, limit = {error_limit}")
 
 
 rng = default_rng()
@@ -52,4 +60,35 @@ test_layer(rng, myml.LinearLayer((1, 1)), (1, 1))
 test_layer(rng, myml.LinearLayer((3, 5)), (5, 1))
 test_layer(rng, myml.Conv2DValidLayer((2, 3, 3, 1)), (1, 4, 6, 1))
 test_layer(rng, myml.Conv2DValidLayer((2, 3, 3, 3)), (2, 4, 6, 3))
+
+test_layer(rng, myml.Sequence([
+    myml.LinearLayer((10, 10)),
+    myml.ReluLayer(),
+]), (10, 2))
+
+test_layer(rng, myml.Sequence([
+    myml.LinearLayer((2, 4)),
+    myml.SoftmaxLayer(),
+]), (4, 3))
+
+test_layer(rng, myml.Sequence([
+    myml.LinearLayer((10, 30)),
+    myml.ReluLayer(),
+    myml.LinearLayer((4, 10)),
+    myml.SoftmaxLayer(),
+]), (30, 2))
+
+test_layer(rng, myml.Sequence([
+    myml.Conv2DValidLayer((32, 3, 3, 1)),
+    myml.ReluLayer(),
+    myml.Conv2DValidLayer((32, 3, 3, 32)),
+    myml.ReluLayer(),
+    myml.FlattenLayer((24, 24, 32)),
+    myml.LinearLayer((625, 24 * 24 * 32)),
+    myml.ReluLayer(),
+    myml.LinearLayer((100, 625)),
+    myml.ReluLayer(),
+    myml.LinearLayer((10, 100)),
+    myml.SoftmaxLayer(),
+]), (1, 28, 28, 1))
 

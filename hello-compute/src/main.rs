f8567e33e56fbc846caf6b216ba695173ed22a12
Jason Orendorff <jorendorff@github.com>
2023-09-16 09:49:48 -0500

    Fix map_slice

--- hello-compute/src/main.rs
+++ hello-compute/src/main.rs
@@ -2,16 +2,21 @@
 
 #[tokio::main]
 async fn main() {
-    let instance = wgpu::Instance::default();
-
     //let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
     //    backends: wgpu::util::backend_bits_from_env().unwrap_or(wgpu::Backends::PRIMARY),
     //    dx12_shader_compiler: wgpu::util::dx12_shader_compiler_from_env().unwrap_or_default(),
     //    gles_minor_version: wgpu::util::gles_minor_version_from_env().unwrap_or_default(),
     //});
 
-    let adapter = wgpu::util::initialize_adapter_from_env(&instance, None)
-        .expect("Couldn't create webgpu adapter");
+    let instance = wgpu::Instance::default();
+
+    //let adapter = wgpu::util::initialize_adapter_from_env(&instance, None)
+    //    .expect("Couldn't create webgpu adapter");
+
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions::default())
+        .await.expect("failed to create wgpu adapter");
+
     let (device, queue) = adapter
         .request_device(
             &wgpu::DeviceDescriptor {
@@ -51,7 +56,7 @@ async fn main() {
         panic!("error scope found something: {}", err);
     }
 
-    let view = map_slice(&readback_buffer.slice(..)).await;
+    let view = map_slice(&device, &readback_buffer.slice(..)).await;
 
     image::save_buffer_with_format(
         "mandelbrot.png",
@@ -64,12 +69,13 @@ async fn main() {
     .expect("error writing image");
 }
 
-async fn map_slice<'a>(slice: &wgpu::BufferSlice<'a>) -> wgpu::BufferView<'a> {
+async fn map_slice<'a>(device: &wgpu::Device, slice: &wgpu::BufferSlice<'a>) -> wgpu::BufferView<'a> {
     let (sender, receiver) = tokio::sync::oneshot::channel();
 
     slice.map_async(wgpu::MapMode::Read, move |result| {
         let _ = sender.send(result);
     });
+    assert!(device.poll(wgpu::Maintain::Wait));
     receiver.await.unwrap().expect("map failed");
     slice.get_mapped_range()
 }
